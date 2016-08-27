fe35b46554b4e6ad1552c4b624892b9e4539f6e0
ivanhao <ivanhao1984@qq.com>
2016-08-28 02:42:08 +0800

    1.0

--- watchdog.sh
+++ watchdog.sh
@@ -11,10 +11,11 @@ do
     y=`ps aux| grep vod_httpserver | grep -v grep | grep -v check | wc -l`  # 计算进程数
     z=`ps aux| grep EmbedThunderManager | grep -v grep | grep -v check | wc -l`  # 计算进程数
     t=`netstat -nap|grep Emb|grep CLOSE|awk 'BEGIN{t=0;} { t++; }  END{print t; }'`  # 有几个连接失败的进程
+    listen=`netstat -nap|grep Emb|grep LISTEN|awk 'BEGIN{t=0;} { t++; }  END{print t; }'`  # 有几个监听的进程
     let c=$x+$y+$z # 进程相加要大于7个
     if ( [ $y -lt $count ] && [ $c -gt 3 ] && [ $x -gt 0 ] && [ $y -gt 0 ] && [ $z -gt 0 ] ) #检测连接失败计数器是否超标，并且进程是否足够
     then
-        if [ $t -lt 4 ] #如果小于四个连接失败
+        if ( [ $t -lt 4 ] && [ $listen -gt 2 ] ) #如果小于四个连接失败，并且监听进程不少于3个
         then
 	    if [ $Islog -eq 1 ];then
                 echo `date "+%Y-%m-%d %H:%M:%S"`" Xware OK!">>$cur_dir/watchdog.log
